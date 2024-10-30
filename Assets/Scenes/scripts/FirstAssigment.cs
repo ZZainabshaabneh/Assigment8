385c0b3f07a94afc1edaea153b4dd3af922066b3
Zainab Shaabneh <zeynepshaabneh32@gmail.com>
2024-10-30 19:51:50 +0300

    first commit

---
+++ Assets/Scenes/scripts/FirstAssigment.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAssigment : MonoBehaviour
+{
+    void Start()
+    {
+        // int number1 = 90;
+        // int number2 = 80;
+        // Debug.Log(number1 + number2);
+        // string name = "zainab";
+        // Debug.Log(name);
+        // Debug.Log(number1 + 190);
+        // Debug.LogWarning("use Upper Case for your name");
+        // Debug.LogError("Enter your Full Name");
+        // int playerLive = 3;
+        // while(playerLive!=0){
+        //     if(playerLive==3){
+        //         Debug.Log("Many lives");
+        //     }
+        //     else
+        //      if (playerLive == 2)
+        //     {
+
+        //     }
+        //     else if (playerLive == 1)
+        //     {
+        //         Debug.Log("The last life!");
+        //     }
+        //     playerLive--;
+        // }
+        // Debug.Log("Game Over!");
+
+        int count = 0;
+        while (count < 20)
+        {
+            int num = Random.Range(1, 20);
+            if (num == 5) continue;
+            if (num == 15) break;
+            Debug.Log(num);
+            count++;
+
+        }
+
+        string[] funny = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+        while (count <= (funny.Length))
+        {
+            int num = Random.Range(1, 8);
+            Debug.Log(funny[num]);
+            count++;
+        }
+    }
+}
